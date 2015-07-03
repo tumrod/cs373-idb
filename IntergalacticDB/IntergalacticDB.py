c0832c1b6439667dd61a948b68d2ca21a151d020
Guillermo Terrazas <gterra023@gmail.com>
2015-07-03 18:48:57 -0500

    Added sort by character route and handle logic

--- IntergalacticDB/IntergalacticDB.py
+++ IntergalacticDB/IntergalacticDB.py
@@ -13,8 +13,14 @@ def about():
     return render_template('about.html')
 
 @app.route('/characters')
-def characters():
-    all_characters = Character.get_all_characters()
+@app.route('/characters/sort_by=<sort_by>')
+def characters(sort_by=None):
+
+    if sort_by is not None:
+        all_characters = Character.get_all_sorted_characters(sort_by)
+    else:
+        all_characters = Character.get_all_characters()
+
     return render_template('characters.html', all_characters=all_characters)
 
 if __name__ == '__main__':
