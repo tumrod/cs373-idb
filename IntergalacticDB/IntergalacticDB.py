3bb849856a3def69a01b51b3cf023e40b7ad42ac
Guillermo Terrazas <gterra023@gmail.com>
2015-07-03 13:42:27 -0500

    Added the characters route and supplied its template with all character data

--- IntergalacticDB/IntergalacticDB.py
+++ IntergalacticDB/IntergalacticDB.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from IntergalacticDB.models import *
 
 app = Flask(__name__)
 
@@ -10,6 +11,11 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/characters')
+def characters():
+    all_chartacters = Character.get_all_characters()
+    return render_template('characters.html', all_chartacters=all_chartacters)
 
 if __name__ == '__main__':
     app.run()
