4ddd05b2756475bac9e831248d37ea667abccc2c
Erin Pennington <epennington@utexas.edu>
2015-07-12 21:30:15 -0500

    fixes #50 species/planet names with space, ' and - should now work

--- static/javascripts/go_button.js
+++ static/javascripts/go_button.js
@@ -1,8 +1,10 @@
 // Manage select value redirects for characters
 $(".generic-select-characters").click(function() {
 
-    var info = (this.id).split('-');
-    var siblingSelect = $('#sort-order-character-' + info[5]);
+    var info = (this.id).split('_');
+    info[5] = info[5].replace(" ", "\\ ");
+    info[5] = info[5].replace("'", "\\'");
+    var siblingSelect = $('#sort_order_character_' + info[5]);
     var selectedValue = siblingSelect.val();
 
     window.location.href = '/characters/' + selectedValue
@@ -10,7 +12,9 @@ $(".generic-select-characters").click(function() {
 
 // Manage select value redirects for species
 $(".generic-select-species").click(function() {
-    var info = (this.id).split('-');
+    var info = (this.id).split('_');
+    info[5] = info[5].replace(" ", "\\ ");
+    info[5] = info[5].replace("'", "\'");
     var siblingSelect = $('#sort-order-species-' + info[5]);
     var selectedValue = siblingSelect.val();
 
